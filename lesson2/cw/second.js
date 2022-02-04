d8f1b9475d770201581fab1a121ebf763f2cc292
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-02-04 19:19:31 +0200

    classwork and homework lesson 2

---
+++ lesson2/cw/second.js
@@ -0,0 +1,61 @@
+// let user_input_1 = +prompt("Please, write your first number");
+// let user_input_2 = +prompt("Please, write your second number");
+// console.log(user_input_1)
+// console.log(user_input_2)
+// let bigger_one = user_input_1<user_input_2 ? user_input_2:user_input_1;
+// console.log(bigger_one)
+// if (user_input_1%2===0) {
+//     console.log("first number is even")
+// } else if (user_input_2%2===0) {
+//     console.log("second number is even")
+// }
+
+// let flat_num = +prompt("Flat number");
+// if (flat_num>0 && flat_num<21){
+//     console.log("You live in first building")
+// } else if (flat_num>=21 && flat_num<49) {
+//     console.log("You live in second building")
+// } else if (flat_num>=49 && flat_num<91) {
+//     console.log("You live in third building")
+// } else {
+//     console.log ("Please put in number from 1 to 90")
+// }
+
+// let number = +prompt("Is this ten?");
+// let check = number===10 ? "Correct":"Incorrect";
+// console.log(check)
+
+
+// // #4
+// let x = +prompt("Write smth");
+// let type_x = typeof x;
+// console.log(type_x)
+// // if(typeof (x) == "number") {
+// //     console.log("1")
+// // }
+
+
+// let wdwd = prompt("do we go to Okten?");
+// let res = wdwd>=10 && wdwd<=22 ? "We go to Okten": "We stay home and study online";
+// console.log(res)
+
+// let guess = +prompt("guess the number from 1 to 5");
+// switch (guess) {
+//     case 1:
+//         console.log("you won a phone")
+//         break;
+//     case 2:
+//         console.log("you won a camera")
+//         break;
+//     case 3:
+//         console.log("you won a drone")
+//         break;
+//     case 4:
+//         console.log("you won a laptop")
+//         break;
+//     case 5:
+//         console.log("you won a bicycle")
+//         break;
+//     default:
+//         console.log("Please put in number from 1 to 5")
+// }
---
+++ lesson2/hw/second.js
@@ -0,0 +1,99 @@
+// let time = prompt("Write in time from 0 to 59");
+// console.log(time)
+// if (time === 0 || time <= 14) {
+//     console.log("First quarter")
+// } else if (time >= 15 && time < 30 ) {
+//     console.log("Second quarter")
+// } else if (time === 30 || time < 45) {
+//     console.log("Third quarter")
+// } else if (time >= 45 && time < 60) {
+//     console.log("Fourth quarter")
+// } else {
+//     document.write("Please put in number from 0 to 59")
+// }
+
+// let day = prompt("Write in day of the month (from 1 to 31)");
+// console.log(day)
+// if (day !== "0" && day < 11 ) {
+//     console.log("First third of month")
+// } else if (day >= 11 && day < 21) {
+//     console.log("Second third of month")
+// } else if (day >= 21 && day < 32) {
+//     console.log("Final third of month")
+// } else {
+//     document.write("Please put in number from 1 to 31")
+// }
+
+// let test = prompt("True or false") ? true:false;
+// console.log(test)
+// let test = !!prompt("True or false");
+// console.log(test)
+// let test = !!prompt("True or false") ? 'Correct':'Incorrect';
+// console.log(test)
+
+// let a = prompt("is a zero?");
+// console.log(a)
+// if (a==="0") {
+//     console.log('Incorrect')
+// } else {
+//     console.log('Correct')
+// }
+
+// let a = +prompt("not a zero?");
+// console.log(a)
+// let not_a_zero = a !== 0 ? "Correct":'Incorrect';
+// console.log(not_a_zero)
+
+// let day_of_a_week = prompt("What day is today?")
+// switch (day_of_a_week) {
+//     case "Monday":
+//         document.write("For Monday you have scheduled this...");
+//         break;
+//     case "Tuesday":
+//         document.write("For Tuesday you have scheduled this...");
+//         break;
+//     case "Wednesday":
+//         document.write("For Wednesday you have scheduled this...");
+//         break;
+//     case "Thursday":
+//         document.write("For Thursday you have scheduled this...");
+//         break;
+//     case "Friday":
+//         document.write("For Friday you have scheduled this...");
+//         break;
+//     case "Saturday":
+//         document.write("For Saturday you have scheduled this...");
+//         break;
+//     case "Sunday":
+//         document.write("For Sunday you have scheduled this...");
+//         break;
+//     default:
+//         document.write("Please write the day of a week i.e. Monday")
+// }
+
+// let year = +prompt("Leap or not? (put in a year)");
+// if (year%100===0) {
+//     console.log("End of century year")
+//     if (year%400===0){
+//         console.log("Leap year")
+//     } else {
+//         console.log("Not a leap year")
+//     }
+// } else if (year%4===0) {
+//     console.log("Leap year")
+// } else {
+//     console.log("Not a leap year")
+// }
+
+// let real_name = prompt("What is a real name of JavaScript?");
+// console.log(real_name)
+// let check = real_name === "ECMAScript" ? "Correct":"Incorrect. It's ECMAScript";
+// console.log(check)
+
+// let real_name = prompt("What is a real name of JavaScript?");
+// console.log(real_name)
+// if (real_name==="ECMAScript") {
+//     console.log("Correct")
+// } else {
+//     console.log("Incorrect. It's ECMAScript")
+// }
