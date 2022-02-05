ae29d0032fcb43fac98cd559a09527d0aaa9c568
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-02-05 22:03:47 +0200

    classwork and homework lesson 3

---
+++ lesson3/hw/third.js
@@ -0,0 +1,103 @@
+// let arr_num = [55, 30,28, 74, 14];
+// let arr_str = ['friday', 'saturday', 'sunday', 'wednesday', 'thursday'];
+// let arr_mix = ['today', 5, true, 'tomorrow', 60];
+// console.log(arr_num, arr_str, arr_mix)
+//
+// let arr_empty = [];
+// arr_empty[0] = 54;
+// arr_empty[1] = 35;
+// arr_empty[2] = 'flow'
+// console.log(arr_empty)
+
+// for (let i =0; i<10; i++) {
+//     document.write(`<div>Lorem ipsum dolor sit amet.</div>`)
+// }
+// for(let i=0; i<10; i++) {
+//     document.write(`<div>${i} Lorem ipsum dolor sit amet.</div>`)
+// }
+
+// let i=0;
+// while (i<20) {
+//     document.write(`<h1>Lorem ipsum dolor sit amet.</h1>`)
+//     i++;
+// }
+
+// let i=0;
+// while (i<20) {
+//     document.write(`<h1>${i} Lorem ipsum dolor sit amet.</h1>`)
+//     i++;
+// }
+
+// let arr_num_1 = [55,44, 30,57, 28, 62, 74, 48, 14, 30];
+// console.log(arr_num_1)
+// for (let n of arr_num_1) {
+//     console.log(n)
+// }
+
+// let arr_str_1 = ['friday', 'saturday', 'sunday', 'wednesday', 'thursday', 'monday', 'tuesday', 'weekend', 'holiday', 'party'];
+// console.log(arr_str_1)
+// for (let s of arr_str_1) {
+//     console.log(s)
+// }
+
+// let arr_mix_1 = ['today', 5, true, 'tomorrow', 60, 'friday', false, 'sunday', 62, 74];
+// console.log(arr_mix_1)
+// for (let element of arr_mix_1) {
+//     console.log(element)
+// }
+
+// let arr_mix_1 = ['today', 5, true, 'tomorrow', 60, 'friday', false, 'sunday', 62, 74];
+// for (let element of arr_mix_1) {
+//     if (typeof element === "boolean"){
+//         console.log(element)
+//     }
+// }
+
+// let arr_mix_1 = ['today', 5, true, 'tomorrow', 60, 'friday', false, 'sunday', 62, 74];
+// for (let element of arr_mix_1) {
+//     if (typeof element === "number"){
+//         console.log(element)
+//     }
+// }
+
+// let arr_mix_1 = ['today', 5, true, 'tomorrow', 60, 'friday', false, 'sunday', 62, 74];
+// for (let element of arr_mix_1) {
+//     if (typeof element === "string"){
+//         console.log(element)
+//     }
+// }
+
+// let arr_mix_2 = ['weekend', false, 'holiday',  48, 'sunday', 'wednesday', 14, true, 30, 'party'];
+// for (let i = 0; i<arr_mix_2.length; i++) {
+//     console.log(arr_mix_2[i])
+// }
+
+// for (let i=0; i<10; i++){
+//     console.log(i)
+//     document.write(i)
+// }
+
+// for (let i=0; i<100; i++) {
+//     console.log(i);
+//     document.write(i)
+// }
+
+// for (let i=0; i<100; i+=2) {
+//     console.log(i)
+//     document.write(i)
+// }
+
+// for (let i=0; i<100; i++) {
+//     if( i%2===0) {
+//         console.log(i)
+//         document.write(i)
+//     }
+// }
+
+// for (let i=0; i<100; i++) {
+//     if( i%2!==0) {
+//         console.log(i)
+//         document.write(i)
+//     }
+// }
+
