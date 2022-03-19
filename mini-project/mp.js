1416725ee97112d82da5d86a15a9b672586ae356
OlhaKuzmyn <olya.kuzmyn@gmail.com>
2022-03-19 12:54:54 +0100

    Mini Project w/out stylization

--- mini-project/mp.js
+++ mini-project/mp.js
@@ -7,6 +7,45 @@
 
 let uCont = document.getElementById('usersContainer')
 
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(usersData => {
+//         for (let userDatum of usersData) {
+//             let divUser = document.createElement('div')
+//             divUser.innerHTML = `
+//             <p>ID: ${userDatum.id}</p>
+//             <h3>Name: ${userDatum.name}</h3>
+//             <h4>Username: ${userDatum.username}</h4>
+//             `
+//             divUser.classList.add('divUser')
+//             uCont.append(divUser)
+//             let btnUser = document.createElement('button')
+//             btnUser.innerText = 'User Details'
+//             divUser.append(btnUser)
+//             btnUser.onclick = function (){
+//                 // let userSession = sessionStorage.getItem('users')
+//                 // if (!userSession){
+//                 //     userSession = []
+//                 //     userSession.push(userDatum)
+//                 //     let strUserS = JSON.stringify(userSession)
+//                 //     sessionStorage.setItem('users', strUserS)
+//                 // } else {
+//                 //     let objUserS = JSON.parse(userSession)
+//                 //     objUserS.push(userDatum)
+//                 //     sessionStorage.setItem('users', JSON.stringify(objUserS))
+//                 // }
+//                 let userSession = []
+//                     userSession.push(userDatum)
+//                     let strUserS = JSON.stringify(userSession)
+//                     sessionStorage.setItem('users', strUserS)
+//                 window.location = `user-details.html?id=${JSON.stringify(userDatum.id)}`
+//             }
+//         }
+//     })
+
+
+// without using array in session storage
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(usersData => {
@@ -23,21 +62,8 @@ fetch('https://jsonplaceholder.typicode.com/users')
             btnUser.innerText = 'User Details'
             divUser.append(btnUser)
             btnUser.onclick = function (){
-                // let userSession = sessionStorage.getItem('users')
-                // if (!userSession){
-                //     userSession = []
-                //     userSession.push(userDatum)
-                //     let strUserS = JSON.stringify(userSession)
-                //     sessionStorage.setItem('users', strUserS)
-                // } else {
-                //     let objUserS = JSON.parse(userSession)
-                //     objUserS.push(userDatum)
-                //     sessionStorage.setItem('users', JSON.stringify(objUserS))
-                // }
-                let userSession = []
-                    userSession.push(userDatum)
-                    let strUserS = JSON.stringify(userSession)
-                    sessionStorage.setItem('users', strUserS)
+                let strUserS = JSON.stringify(userDatum)
+                sessionStorage.setItem('user', strUserS)
                 window.location = `user-details.html?id=${JSON.stringify(userDatum.id)}`
             }
         }
--- mini-project/pd.js
+++ mini-project/pd.js
@@ -5,9 +5,96 @@
 // (эндпоинт для получения информации - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 
 
-// document.body.onload = function (){
-//     sessionStorage.clear()
-// }
+document.body.onload = function (){
+    sessionStorage.clear()
+}
 
 let postContainer = document.getElementById('postContainer')
-let postSelect = JSON.parse(sessionStorage.getItem('posts'))
+
+// let postSelect = JSON.parse(sessionStorage.getItem('posts'))
+// for (let postElement of postSelect) {
+//     let postSel = document.createElement('div')
+//     for (let postKey in postElement) {
+//         if (postKey === 'title'){
+//             let htitle = document.createElement('h3')
+//             htitle.innerText = `${postKey.charAt(0).toUpperCase() + postKey.slice(1)}: ${postElement[postKey]}`
+//             postSel.append(htitle)
+//         } else {
+//             let pDetail = document.createElement('p')
+//             pDetail.innerText = `${postKey.charAt(0).toUpperCase() + postKey.slice(1)}: ${postElement[postKey]}`
+//             postSel.append(pDetail)
+//         }
+//     }
+//     postContainer.append(postSel)
+//     let btnComments = document.createElement('button')
+//     btnComments.innerText = 'Show comments'
+//     btnComments.onclick = function (){
+//         fetch('https://jsonplaceholder.typicode.com/posts/'+postElement.id+'/comments')
+//             .then(response => response.json())
+//             .then(comments => {
+//                 for (let comment of comments) {
+//                     let commDiv = document.createElement('div')
+//                     for (let commentKey in comment) {
+//                         if (commentKey === 'name'){
+//                             let hName = document.createElement('h4')
+//                             hName.innerText = `${commentKey.charAt(0).toUpperCase() + commentKey.slice(1)}: ${comment[commentKey]}`
+//                             commDiv.append(hName)
+//                         } else {
+//                             let pDetail = document.createElement('p')
+//                             pDetail.innerText = `${commentKey.charAt(0).toUpperCase() + commentKey.slice(1)}: ${comment[commentKey]}`
+//                             commDiv.append(pDetail)
+//                         }
+//                     }
+//                     postSel.append(commDiv)
+//                     btnComments.disabled = true
+//                 }
+//             })
+//     }
+//     postSel.append(btnComments)
+// }
+
+//without using array in session storage
+
+let postSelect = JSON.parse(sessionStorage.getItem('post'))
+
+let postSel = document.createElement('div')
+for (let postKey in postSelect) {
+    if (postKey === 'title'){
+        let htitle = document.createElement('h3')
+        htitle.innerText = `${postKey.charAt(0).toUpperCase() + postKey.slice(1)}: ${postSelect[postKey]}`
+        postSel.append(htitle)
+    } else {
+        let pDetail = document.createElement('p')
+        pDetail.innerText = `${postKey.charAt(0).toUpperCase() + postKey.slice(1)}: ${postSelect[postKey]}`
+        postSel.append(pDetail)
+    }
+}
+postContainer.append(postSel)
+let btnComments = document.createElement('button')
+btnComments.innerText = 'Show comments'
+btnComments.onclick = function (){
+    fetch('https://jsonplaceholder.typicode.com/posts/'+postSelect
+
+
+        .id+'/comments')
+        .then(response => response.json())
+        .then(comments => {
+            for (let comment of comments) {
+                let commDiv = document.createElement('div')
+                for (let commentKey in comment) {
+                    if (commentKey === 'name'){
+                        let hName = document.createElement('h4')
+                        hName.innerText = `${commentKey.charAt(0).toUpperCase() + commentKey.slice(1)}: ${comment[commentKey]}`
+                        commDiv.append(hName)
+                    } else {
+                        let pDetail = document.createElement('p')
+                        pDetail.innerText = `${commentKey.charAt(0).toUpperCase() + commentKey.slice(1)}: ${comment[commentKey]}`
+                        commDiv.append(pDetail)
+                    }
+                }
+                postSel.append(commDiv)
+                btnComments.disabled = true
+            }
+        })
+}
+postSel.append(btnComments)
